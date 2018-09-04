f5383195cc4b17d61d73c90e74250386bca0a7f9
Georvi Vlasakiev <g.vlasakiev@gmail.com>
2018-09-04 16:42:42 +0300

    Initial commit

---
+++ src/main/java/com/project/shoecatalogueappspring/ShoeCatalogueAppSpringApplication.java
@@ -0,0 +1,12 @@
+package com.project.shoecatalogueappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeCatalogueAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeCatalogueAppSpringApplication.class, args);
+    }
+}
